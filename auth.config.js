ef4e8c4e11469d157e036a4d646c49f336e8b746
jaehanjung <wjdwogks23@gmail.com>
2018-06-01 16:40:32 +0900

    장바구니 db기능 추가

--- auth.config.js
+++ auth.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   products: {
     write: 'ownerOnly'
+  },
+  carts: {
+    read: 'ownerOnly',
+    write: 'ownerOnly'
   }
-
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { freemem } from "os";
 
 const postAPI = axios.create({
   baseURL: process.env.API_URL
@@ -6,9 +7,15 @@ const postAPI = axios.create({
 const rootEl = document.querySelector(".root");
 
 function login(token) {
+  console.log(`login: ${token}`)
   localStorage.setItem("token", token);
   postAPI.defaults.headers["Authorization"] = `Bearer ${token}`;
   rootEl.classList.add("root--authed");
+}
+function logout() {
+  localStorage.removeItem("token");
+  delete postAPI.defaults.headers["Authorization"];
+  rootEl.classList.remove("root--authed");
 }
 // 중복코드 함수로 작성
 function render(fragment) {
@@ -23,7 +30,8 @@ const templates = {
   signup: document.querySelector("#Sign-Up").content,
   Detail: document.querySelector("#Detail-page").content,
   detailMore: document.querySelector("#Detail-more").content,
-  cart: document.querySelector("#cart-page").content
+  cart: document.querySelector("#cart-page").content,
+  cartMore: document.querySelector("#cart-list").content
 };
 
 // 메인페이지
@@ -36,29 +44,33 @@ async function indexPage() {
       loginPage();
     });
   listFragment
+    .querySelector(".post-list__logout-btn")
+    .addEventListener("click", e => {
+      logout();
+      indexPage();
+    });
+  listFragment
     .querySelector(".signup-page__btn")
     .addEventListener("click", e => {
       signupPage();
     });
-  listFragment.querySelector(".card-more").addEventListener("click", e => {
-    DetailPage();
-  });
+
   listFragment.querySelector(".cart-page__btn").addEventListener("click", e => {
     cartPage();
   });
   res.data.forEach(post => {
     const fragment = document.importNode(templates.box, true);
-    fragment.querySelector('.post-item__author').textContent = post.product
+    fragment.querySelector(".post-item__author").textContent = post.product;
     const pEl = fragment.querySelector(".post-item__title");
     pEl.textContent = post.price;
     const itemEL = fragment.querySelector(".post-item__img");
-    itemEL.setAttribute("src",post.imgURL)
-    // pEl.addEventListener("click", e => {
-    //   postContentPage(post.id);
-    // });
+    itemEL.setAttribute("src", post.imgURL);
+    const boxEl = fragment.querySelector(".card-box");
+    boxEl.addEventListener("click", e => {
+      DetailPage(post.id);
+    });
     listFragment.querySelector(".post-list").appendChild(fragment);
   });
-
 
   render(listFragment);
 }
@@ -73,10 +85,10 @@ async function loginPage() {
       password: e.target.elements.password.value
     };
     e.preventDefault();
-    const res = await postAPI.post("/users/", payload);
+    const res = await postAPI.post("/users/login", payload);
     login(res.data.token);
     indexPage();
-  })
+  });
   fragment.querySelector(".logo").addEventListener("click", e => {
     indexPage();
   });
@@ -84,41 +96,94 @@ async function loginPage() {
   render(fragment);
 }
 //회원가입 페이지
+
 async function signupPage() {
   const fragment = document.importNode(templates.signup, true);
+  const formEl = fragment.querySelector(".Sign_form");
+  formEl.addEventListener("submit", async e => {
+    // e.target.elements.username === fragment.querySelector('.login__username');
+    const payload = {
+      username: e.target.elements.username.value,
+      password: e.target.elements.password.value,
+    };
+    e.preventDefault();
+    const res = await postAPI.post("/users/register", payload);
+    login(res.data.token);
+    const meRes = await postAPI.get("/me");
+    const userPayload = {
+      name: e.target.elements.name.value,
+      address: e.target.elements.address.value,
+      mobile: e.target.elements.mobile.value
+    }
+    const userRes = await postAPI.patch(`/users/${meRes.data.id}`, userPayload);
+    indexPage();
+  });
   fragment.querySelector(".logo").addEventListener("click", e => {
     indexPage();
   });
   render(fragment);
 }
 // 상품상세 페이지
-async function DetailPage() {
-  const res = await postAPI.get("/products");
+async function DetailPage(id) {
+  const res = await postAPI.get(`/products/${id}`);
   const listFragment = document.importNode(templates.Detail, true);
   listFragment.querySelector(".logo").addEventListener("click", e => {
     indexPage();
   });
-  res.data.forEach(post => {
-    const fragment = document.importNode(templates.detailMore, true);
-    fragment.querySelector('.Detail-title').textContent = post.product
-    const pEl = fragment.querySelector(".Detail-price");
-    pEl.textContent = post.price;
-    const itemEL = fragment.querySelector(".Detail__img");
-    itemEL.setAttribute("src",post.imgURL)
-    // pEl.addEventListener("click", e => {
-    //   postContentPage(post.id);
-    // });
-    listFragment.querySelector(".Detail-list").appendChild(fragment);
+  const fragment = document.importNode(templates.detailMore, true);
+  const formEl = fragment.querySelector(".Detail-form");
+
+  formEl.addEventListener("submit", async e => {
+    e.preventDefault();
+    // e.target.elements.username === fragment.querySelector('.login__username');
+    const payload = {
+      productId: id
+    };
+    e.preventDefault();
+    const res = await postAPI.post("/carts", payload);
+    cartPage();
   });
+
+  const { product, price, imgURL, content, imgsize } = res.data;
+  fragment.querySelector(".Detail-title").textContent = product;
+  fragment.querySelector(".Detail-title_title").textContent = product;
+  fragment.querySelector(".Detail-text").textContent = content;
+  const pEl = fragment.querySelector(".Detail-price");
+  pEl.textContent = price;
+  const itemEL = fragment.querySelector(".Detail__img");
+  itemEL.setAttribute("src", imgURL);
+  const imgsizeEl = fragment.querySelector(".Detail__imgsize");
+  imgsizeEl.setAttribute("src", imgsize);
+
+  listFragment.querySelector(".Detail-list").appendChild(fragment);
+
+
   render(listFragment);
 }
 
 // 장바구니 페이지
 async function cartPage() {
-  const fragment = document.importNode(templates.cart, true);
-  fragment.querySelector(".logo").addEventListener("click", e => {
+  const res = await postAPI.get(`/carts?_expand=product`);
+  const listFragment = document.importNode(templates.cart, true);
+  listFragment.querySelector(".logo").addEventListener("click", e => {
     indexPage();
   });
-  render(fragment);
+  res.data.forEach(cart => {
+    const fragment = document.importNode(templates.cartMore, true);
+    fragment.querySelector(".cart-title").textContent = cart.product.product;
+    const pEl = fragment.querySelector(".cart-price");
+    pEl.textContent = cart.product.price;
+    const itemEL = fragment.querySelector(".cart-img");
+    itemEL.setAttribute("src", cart.product.imgURL);
+    const totalEl = fragment.querySelector(".cart-price_total");
+    totalEl.textContent = cart.product.price;
+    listFragment.querySelector(".cart-list").appendChild(fragment);
+  });
+  render(listFragment);
 }
+
+if (localStorage.getItem("token")) {
+  login(localStorage.getItem("token"));
+}
+
 indexPage();
